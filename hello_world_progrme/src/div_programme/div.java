e2500eacb26ef2c1e1b100ba7c378352c7831149
Sazidul Rahman <100462697+sazidulcse@users.noreply.github.com>
2022-05-01 16:03:41 +0600

    this is fast commit in github repo

--- hello_world_progrme/src/div_programme/div.java
+++
@@ -1,7 +0,0 @@
-package div_programme;
-
-public class div {
-  public static void main(String[] args) {
-      System.out.println("hello world div ");
-  }  
-}
--- hello_world_progrme/src/hello_world_progrme/Hello_world_progrme.java
+++ hello_world_progrme/src/hello_world_progrme/Hello_world_progrme.java
@@ -9,13 +9,12 @@ public class Hello_world_progrme {
 
    
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO HELLO WORLD PROGRAMME HARE
+String word= "Hello World";
 
 System.out.println("hello world. ");
-int num, num3;
-num=33; num3=33;
-int result = num + num3; 
-System.out.println(result);
+System.out.println(word);
+
 
     }
     
